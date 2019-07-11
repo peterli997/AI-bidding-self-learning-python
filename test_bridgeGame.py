cae41096809e46a632f36e761c31bd9a8bafaa21
Dun Ma <madun1999@gmail.com>
2019-07-11 17:58:03 +0800

    add test for bridge simulator

---
+++ test_bridgeGame.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+
+
+class TestBridgeGame(TestCase):
+    def test_create_random_board(self):
+        self.fail()
+
+    def test_create_random_board(self):
+        self.fail()
+
+    def test_create_random_board(self):
+        self.fail()
+
+    def test_calculate_score(self):
+        self.fail()
+
+    def test_is_greater_bid(self):
+        self.fail()
+
+    def test_validate_bid(self):
+        self.fail()
+
+    def test_is_done_bidding(self):
+        self.fail()
+
+    def test_is_done_playing(self):
+        self.fail()
+
+    def test_get_contract(self):
+        self.fail()
+
+    def test_card_value_key(self):
+        self.fail()
+
+    def test_get_trick_winner(self):
+        self.fail()
