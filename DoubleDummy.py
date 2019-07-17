255c19c8d8f52ec973cc0ffb64df7ffe41887642
peterli997 <peterli997@sina.com>
2019-07-17 10:17:40 +0800

    should return 7 tricks but returns 5?

--- DoubleDummy.py
+++ DoubleDummy.py
@@ -14,6 +14,12 @@ Higher ranks are assigned higher codes. 2 of Club is assigned 0.
 
 
 def comp(a, b):
+    """
+    compares if card ranks a and b belong to the same suit
+    :param a: card rank of card one
+    :param b: card rank of card two
+    :return: boolean True or False indicating whether the cards belong to the same suit
+    """
     return a//13 == b//13
 
 
@@ -33,6 +39,7 @@ def MAX_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):  # trump: C = 1, D =
                     winning_card = state[i][0]
         if card_holder_dict[winning_card] % 2 == 0:
             NS += 1
+        print(state, trump, alpha, beta, NS, EW, NS, "1")
         return NS
     else:
         if m % 4 == 0:
@@ -63,9 +70,10 @@ def MAX_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):  # trump: C = 1, D =
                         flag = True
                         NS += 1
                     else:
-                        EW += 1
+                        EW -= 1
                     if NS > alpha:
                         alpha = NS
+                        print("alpha = NS, ", alpha)
                         if alpha >= beta:
                             return alpha
                     if EW < beta:
@@ -82,14 +90,14 @@ def MAX_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):  # trump: C = 1, D =
                         s = [t, s[1], s[2], s[3]]
                 if flag:
                     alpha = max(alpha, MAX_VALUE(s, trump, alpha, beta, NS, EW))
+                    # print("alpha = max(max), ", alpha, s, trump, alpha, beta, NS, EW)
                 else:
                     alpha = max(alpha, MIN_VALUE(s, trump, alpha, beta, NS, EW))
-                if l <= 8:
-                    finish = time.time()
-                    print(finish - start)
-                    quit()
+                    # print("alpha = max(min), ", alpha, s, trump, alpha, beta, NS, EW)
                 if alpha >= beta:
+                    print(state, trump, alpha, beta, NS, EW, beta, "2")
                     return beta
+    print(state, trump, alpha, beta, NS, EW, alpha, "3")
     return alpha
 
 
@@ -107,8 +115,9 @@ def MIN_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):
             else:
                 if (comp(state[i][0], winning_card) and play[i] > winning_card) or (state[i][0] // 13 == trump - 1 and winning_card // 13 != trump - 1):
                     winning_card = state[i][0]
-        if card_holder_dict[winning_card] % 2 == 0:
+        if card_holder_dict[winning_card] % 2 != 0:
             EW -= 1
+        print(state, trump, alpha, beta, NS, EW, EW, "4")
         return EW
     else:
         if m % 4 == 0:
@@ -139,7 +148,7 @@ def MIN_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):
                         flag = True
                         NS += 1
                     else:
-                        EW += 1
+                        EW -= 1
                     if NS > alpha:
                         alpha = NS
                         if alpha >= beta:
@@ -160,16 +169,25 @@ def MIN_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):
                     beta = min(beta, MAX_VALUE(s, trump, alpha, beta, NS, EW))
                 else:
                     beta = min(beta, MIN_VALUE(s, trump, alpha, beta, NS, EW))
-                if l <= 8:
-                    finish = time.time()
-                    print(finish - start)
-                    quit()
                 if alpha >= beta:
+                    print(state, trump, alpha, beta, NS, EW, alpha, "5")
                     return alpha
+    print(state, trump, alpha, beta, NS, EW, beta, "6")
     return beta
+"""
+Above is the minimax algorithm, showing that on NS perspective, North and South (dummy controlled by North) aims to
+maximize NS tricks, where East and West aims to minimize EW tricks. Both MAX_VALUE and MIN_VALUE returns the number of
+NS tricks. MAX_VALUE returns at this state, given EW's best effort to minimize NS tricks, how many tricks can NS get.
+MIN_VALUE returns at this state, at most how many tricks can NS get with all possible plays (potentially not with any
+effort towards the bridge game's goals).
+Alpha-beta algorithm is an algorithm that skips unnecessary nodes in a bridge game that aims to optimize the algorithm.
+The effectiveness of the optimization from the alpha-beta algorithm depends on how the tree is approached with the
+algorithm, which in turn depends on the actual distribution of hands. Nevertheless, alpha-beta will skip nodes and
+should be faster than minimax algorithm itself.
+"""
 
 
-def get_suit(card):  # suit function
+def get_suit(card):
     """
     Get suit from card code
     :param card: card code
@@ -299,6 +317,27 @@ def input_hands_from_file(filename, number_of_hands):
             hand_count += 1
     return hands
 
+play = [-1] * 52
+card_holder = [-1] * 52
+card_rank = list(range(52))
+s = [19, 21, 24, 31, 32, 36]
+w = [9, 13, 15, 16, 29, 33]
+n = [14, 20, 22, 25, 28, 38]
+e = [5, 10, 17, 18, 23, 34]
+for j in s:
+    card_holder[j] = 0
+for j in w:
+    card_holder[j] = 1
+for j in n:
+    card_holder[j] = 2
+for j in e:
+    card_holder[j] = 3
+card_holder_dict = dict(zip(card_rank, card_holder))
+start = time.time()
+print(MAX_VALUE(state=[s, w, n, e], trump=5, alpha=0, beta=len(n), NS=0, EW=len(n)))
+finish = time.time()
+print(finish - start)
+quit()
 
 RC = set(range(52))  # RC for remaining cards
 if INPUT_METHOD == 0:
@@ -318,10 +357,11 @@ RC = RC - set(Sindex)
 RC = list(RC)
 RC2 = RC.copy()
 
-print("S")
-print(hand_to_string(Sindex))
 print("N")
 print(hand_to_string(Nindex))
+print("S")
+print(hand_to_string(Sindex))
+
 
 for count in range(5):
     RC = RC2
@@ -338,24 +378,26 @@ for count in range(5):
     print()
     print("E")
     print(hand_to_string(Eindex))
-    card_holder = np.zeros(52, dtype=int)
-    card_rank = np.arange(52)
-    for j in Nindex:
+    card_holder = [-1] * 52
+    card_rank = list(range(52))
+    for j in Sindex:
         card_holder[j] = 0
     for j in Windex:
         card_holder[j] = 1
-    for j in Sindex:
+    for j in Nindex:
         card_holder[j] = 2
     for j in Eindex:
         card_holder[j] = 3
     card_holder_dict = dict(zip(card_rank, card_holder))
+    # card_holder_dict is a dictionary that sends every card to the player that holds it in the beginning
     play = [-1] * 52
+    # play stores card codes of cards that have been played in order, and -1 represents haven't reached that turn yet
 
     # Case 1: Trump = NT
     # print(type(Windex), type(Sindex), type(Eindex), type(Nindex))
-    current_state = [Windex, Sindex, Eindex, Nindex]
+    current_state = [Windex, Nindex, Eindex, Sindex]
     start = time.time()
-    print(MAX_VALUE(state=current_state, trump=5), "NT")
+    print(MAX_VALUE(state=current_state, trump=5, alpha=0, beta=13, NS=0, EW=13), "NT")
     # Case 2：Trump = C
     start = time.time()
     print(MAX_VALUE(state=current_state, trump=1), "C")
