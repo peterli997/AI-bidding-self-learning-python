44e89d24e90dbfb0d0905e2e7326c8f81b81931a
Dun Ma <madun1999@gmail.com>
2019-07-19 01:36:51 +0800

    compressed lookup table

--- DoubleDummy.py
+++ DoubleDummy.py
@@ -4,6 +4,7 @@ from itertools import permutations
 import os
 import collections
 import pickle
+import profile
 
 INPUT_METHOD = 1  # 0 for console, 1 for file
 INPUT_FILE_NAME = "input.txt"  # file name for input
@@ -12,9 +13,11 @@ TRICK_LOOKUP_TABLE = False # if using trick lookup table
 
 LINK_LEVEL = 7 # number of remaining tricks to be stored - 1
 HASH_MOD = [64,65536,536870912,8589934592,4611686018427387904,4611686018427387904,4611686018427387904,4611686018427387904]
+DETAILED_LINK_OBJ = False # if links are stored
+
 Suit = ['S', 'H', 'D', 'C']
 Card = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14']
-TEST_L = -1  # -1 for complete run
+TEST_L = 16  # -1 for complete run
 """
 Card code ranges from 0 to 51,
 Order: Higher suits are assigned higher codes. Clubs are assigned 0-12, diamonds 13-25, hearts 26-38, and spades 39-51.
@@ -24,95 +27,99 @@ Higher ranks are assigned higher codes. 2 of Club is assigned 0.
 
 class SuitLink:
     def __init__(self, link):
-        self.compressed_link = list(filter(lambda x: x != -1, link))
-        self.cache_valid = False
-        self.cache_hash = self.__hash__()
-        self.cache_valid = True
+        if DETAILED_LINK_OBJ:
+            self.compressed_link = list(filter(lambda x: x != -1, link))
+            self.cache_hash = self.hashing(self.compressed_link)
+        else:
+            compressed_link = list(filter(lambda x: x != -1, link))
+            self.cache_hash = self.hashing(compressed_link)
 
     def __str__(self):
-        return str(self.compressed_link)
+        return str(self.compressed_link) if DETAILED_LINK_OBJ else str(self.cache_hash)
+
+    @staticmethod
+    def hashing(comp_link):
+        hashed = 0
+        for i in comp_link:
+            hashed = hashed * 4 + i
+        return hashed * 17 + len(comp_link) - 1
 
     def __hash__(self):  # different hash => different link
-        if self.cache_valid:
-            return self.cache_hash
-        hashed = 0
-        for i in self.compressed_link:
-            if i != -1:
-                hashed = hashed * 4 + i
-        self.cache_hash = hashed * 13 + len(self.compressed_link) - 1
-        self.cache_valid = True
         return self.cache_hash
 
     def __eq__(self, other):
-        if self.cache_valid and other.cache_valid:
-            if self.cache_hash == other.cache_hash:
-                assert self.compressed_link == other.compressed_link
-                return True
-            else:
-                assert self.compressed_link != other.compressed_link
-                return False
-        else:
-            return self.compressed_link == other.compressed_link
+        return self.cache_hash == other.cache_hash
+
+    def __bool__(self):
+        return self.cache_hash != -1
+
+    def __len__(self):
+        return (self.cache_hash+1) % 17
 
 
 class SuitLevelLinks:
     def __init__(self, link_dict, trump, leader=0):
-        self.cache_valid = False
-        self.trump = trump
         if leader != 0:
-            self.link_dict = set()
+            links = set()
+            link_trump = SuitLink([])
             for i, link in link_dict.items():
                 if i == trump - 1:
-                    self.link_trump = SuitLink([(x - leader) % 4 for x in link if x != -1])
+                    link_trump = SuitLink([(x - leader) % 4 for x in link if x != -1])
                 else:
-                    self.link_dict.add(SuitLink([(x - leader) % 4 for x in link if x != -1]))
+                    links.add(SuitLink([(x - leader) % 4 for x in link if x != -1]))
         else:
-            self.link_dict = {SuitLink(y) for x, y in link_dict.items() if x != trump - 1}
-            if trump != 5:
-                self.link_trump = SuitLink(link_dict[trump-1])
-        self.cache_link_hash = {x.__hash__() for x in self.link_dict}  # need to be alwsys valid
-        if trump != 5:
-            self.cache_trump_hash = self.link_trump.__hash__()  # need to be always valid
-        self.cache_hash = self.__hash__()
-        self.cache_valid = True
+            links = {SuitLink(y) for x, y in link_dict.items() if x != trump - 1}
+            link_trump = SuitLink(link_dict[trump-1]) if trump != 5 else SuitLink([])
+        if DETAILED_LINK_OBJ:
+            self.links = (links, link_trump)
+        self.cache_links_hash = ({x.__hash__() for x in links}, link_trump.__hash__())
+        self.cache_hash = self.hashing(self.cache_links_hash)
 
     def __str__(self):
-        return str(self.link_dict) + str(self.trump) + "" if self.trump == 5 else str(self.link_trump)
+        if DETAILED_LINK_OBJ:
+            result = "{"
+            for i in self.links[0]:
+                result += str(i)
+                result += ', '
+            result += '}'
+            return result + " trump:" + str(self.links[1])
+        else:
+            return str(self.cache_links_hash)
+
+    @staticmethod
+    def hashing(links_hash):
+        hashed = links_hash[1]
+        for i in sorted(links_hash[0]):
+            hashed = (hashed * 872415239 + i) % HASH_MOD[LINK_LEVEL]  # it is a prime above max hash of link
+        return hashed
 
     def __hash__(self):
-        if self.cache_valid:
-            return self.cache_hash
-        else:
-            hashed = 0
-            if self.trump != 5:
-                hashed += self.cache_trump_hash
-            for i in sorted(self.cache_link_hash):
-                hashed = (hashed * 872415239 + i) % HASH_MOD[LINK_LEVEL]  # it is a prime above max hash of link
-            if self.trump == 5:
-                hashed *= 3
-            self.cache_hash = hashed
-            self.cache_valid = True
-            return hashed
+        return self.cache_hash
 
     def __eq__(self, other):
-        if self.trump != other.trump:
+        if self.cache_hash != other.cache_hash:
             return False
-        if self.cache_valid and other.cache_valid and self.cache_hash != other.cache_hash:
-            return False
-        if self.cache_link_hash != other.cache_link_hash:
-            return False
-        if self.trump != 5:
-            if self.cache_trump_hash != other.cache_trump_hash:
-                return False
-        return True
+        return self.cache_links_hash == other.cache_links_hash
+
+    def __len__(self):
+        result = 0
+        for i in self.cache_links_hash[0]:
+            result += (i+1) % 17
+        return result + (1 + self.cache_links_hash[1]) % 17
 
 
 def comp(a, b):
     return a//13 == b//13
+
+
 def pickle_dump_link_lookup_table():
-    f = open("link_lookup_table.pkl", 'wb+')
+    if DETAILED_LINK_OBJ:
+        f = open("detailed_link_lookup_table.pkl", 'wb+')
+    else:
+        f = open("compressed_link_lookup_table.pkl", 'wb+')
     pickle.dump(link_lookup_table, f, pickle.HIGHEST_PROTOCOL)
     f.close()
+
 
 def update_link_lookup_table(suit_level_links, NS_min, EW_min):
     global link_lookup_table
@@ -123,6 +130,7 @@ def update_link_lookup_table(suit_level_links, NS_min, EW_min):
         link_lookup_table[suit_level_links] = (NS_min1, EW_min1)
     else:
         link_lookup_table[suit_level_links] = (NS_min, EW_min)
+
 
 def MAX_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):  # trump: C = 1, D = 2, H = 3, S = 4, NT = 5
     global play
@@ -151,11 +159,14 @@ def MAX_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):  # trump: C = 1, D =
                     if remaining_EW + remaining_NS == m // 4:
                         assert NS + remaining_NS == EW - remaining_EW, "NS EW should be the same if" \
                             " remaining is determined" + str(remaining_NS) + str(remaining_EW)
+                        # print("max, read from lookup table, completely searched", state, remaining_NS, remaining_EW)
                         return NS + remaining_NS
                     if remaining_NS + NS >= beta:      # remaining_NS + NS = alpha
+                        # print("max, read from lookup table, partially searched", state, remaining_NS, remaining_EW)
                         return remaining_NS + NS
                     # EW -= m // 4 - remaining_tricks
                     if EW - remaining_EW <= alpha:  # EW - m % 4 + remaining_EW = beta
+                        # print("max, read from lookup table, partially searched", state, remaining_NS, remaining_EW)
                         return alpha
             playable_cards = state[0].copy()
         else:
@@ -167,11 +178,13 @@ def MAX_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):  # trump: C = 1, D =
             if k % 13 == 0 or k - 1 not in playable_cards:
                 play[52 - m] = k
                 state[0].remove(k)                    # remove from state
+                # print("max-in", state, k)
                 assert suit_level_links[k//13][k % 13] == cur_player
                 suit_level_links[k//13][k % 13] = -1  # remove from suit_level_links
                 if m % 4 != 1:   # not end of trick
                     s = [state[1], state[2], state[3], state[0]]
                     alpha = max(alpha, MIN_VALUE(s, trump, alpha, beta, NS, EW))
+                    # print("max-out", state, k)
                 else:     # end of trick
                     if TRICK_LOOKUP_TABLE:
                         winner = int(trick_lookup_table[trump-1][play[l]][play[l+1]][play[l+2]][play[l+3]])
@@ -184,12 +197,14 @@ def MAX_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):  # trump: C = 1, D =
                             if alpha >= beta:
                                 state[0].add(k)
                                 suit_level_links[k // 13][k % 13] = cur_player
+
                                 return alpha
                         if winner == 3:
                             s = state[:]
                         else:
                             s = state[2:] + state[:2]
                         alpha = max(alpha, MAX_VALUE(s, trump, alpha, beta, NS, EW))
+                        # print("max-out", state, k)
                         NS -= 1
                     else:
                         EW -= 1
@@ -198,9 +213,11 @@ def MAX_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):  # trump: C = 1, D =
                             if alpha >= beta:
                                 state[0].add(k)
                                 suit_level_links[k // 13][k % 13] = cur_player
+
                                 return alpha
                         s = state[winner + 1:] + state[:winner + 1]
                         alpha = max(alpha, MIN_VALUE(s, trump, alpha, beta, NS, EW))
+                        # print("max-out", state, k)
                         EW += 1
                 if l <= TEST_L:
                     finish = time.time()
@@ -244,15 +261,18 @@ def MIN_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):
             if m <= LINK_LEVEL * 4 + 4:
                 links = SuitLevelLinks(suit_level_links, trump, cur_player)
                 if links in link_lookup_table:
-                    remaining_NS, remaining_EW = link_lookup_table[links]
+                    remaining_EW, remaining_NS = link_lookup_table[links]
                     if remaining_EW + remaining_NS == m // 4:
                         assert NS + remaining_NS == EW - remaining_EW, "NS EW should be the same if" \
                                                                                   " remaining is determined"
+                        # print("min, read from lookup table, completely searched", state, remaining_EW, remaining_NS)
                         return NS + remaining_NS
                     if remaining_NS + NS >= beta:      # remaining_NS + NS = alpha
+                        # print("min, read from lookup table, partially searched", state, remaining_EW, remaining_NS)
                         return beta
                     # EW -= m // 4 - remaining_tricks
                     if EW - remaining_EW <= alpha:  # EW - m % 4 + remaining_EW = beta
+                        # print("min, read from lookup table, partially searched", state, remaining_EW, remaining_NS)
                         return EW - remaining_EW
             playable_cards = state[0].copy()
         else:
@@ -264,11 +284,13 @@ def MIN_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):
             if k % 13 == 0 or k - 1 not in playable_cards:
                 play[52 - m] = k
                 state[0].remove(k)
+                # print("min-out", state, k)
                 assert suit_level_links[k // 13][k % 13] == cur_player
                 suit_level_links[k // 13][k % 13] = -1  # remove from suit_level_links
                 if m % 4 != 1:
                     s = [state[1], state[2], state[3], state[0]]
                     beta = min(beta, MAX_VALUE(s, trump, alpha, beta, NS, EW))
+                    # print("min-out", state, k)
                 else:
                     if TRICK_LOOKUP_TABLE:
                         winner = int(trick_lookup_table[trump-1][play[l]][play[l+1]][play[l+2]][play[l+3]])
@@ -284,6 +306,7 @@ def MIN_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):
                                 return beta
                         s = state[winner + 1:] + state[:winner + 1]
                         beta = min(beta, MIN_VALUE(s, trump, alpha, beta, NS, EW))
+                        # print("min-out", state, k)
                         EW += 1
                     else:
                         NS += 1
@@ -298,6 +321,7 @@ def MIN_VALUE(state, trump, alpha=0, beta=13, NS=0, EW=13):
                         else:
                             s = state[:]
                         beta = min(beta, MAX_VALUE(s, trump, alpha, beta, NS, EW))
+                        # print("min-in", state, k)
                         NS -= 1
                 if l <= TEST_L:
                     finish = time.time()
@@ -487,94 +511,109 @@ def create_lookup_table():
     np.save("trick_lookup_table", trick_lookup_table)
     # finish = time.time()
     # print("save using np.save() time:", finish - start)
+suit_level_links = []
+def main():
+    global play
+    global card_holder_dict
+    global suit_level_links
+    global link_lookup_table
 
+    RC = set(range(52))  # RC for remaining cards
+    if INPUT_METHOD == 0:
+        Nindex = input_hand_from_console("North")
+        Sindex = input_hand_from_console("South")
 
-RC = set(range(52))  # RC for remaining cards
-if INPUT_METHOD == 0:
-    Nindex = input_hand_from_console("North")
-    Sindex = input_hand_from_console("South")
-
-elif INPUT_METHOD == 1:
-    hands = input_hands_from_file(INPUT_FILE_NAME, 4)
-    Nindex = hands[0]
-    Eindex = hands[1]
-    Sindex = hands[2]
-    Windex = hands[3]
-else:
-    Nindex = set(range(13))
-    Sindex = set(range(14, 26))
-
-print("S")
-print(hand_to_string(Sindex))
-print("N")
-print(hand_to_string(Nindex))
-
-if INPUT_METHOD != 1:
-    RC = RC - Nindex
-    RC = RC - Sindex
-    RC = RC
-    RC2 = RC.copy()
-    RC = np.array(list(RC2))
-    Windex = np.random.choice(a=RC, size=13, replace=False)
-    Windex.sort()
-    Windex = Windex.tolist()
-    Windex = set(Windex)
-    RC = set(RC) - Windex
-    Eindex = RC
-
-
-print("W")
-print(hand_to_string(Windex))
-print("E")
-print(hand_to_string(Eindex))
-if TRICK_LOOKUP_TABLE:
-    if os.path.exists("trick_lookup_table.npy"):
-        trick_lookup_table = np.load("trick_lookup_table.npy")
+    elif INPUT_METHOD == 1:
+        hands = input_hands_from_file(INPUT_FILE_NAME, 4)
+        Nindex = hands[0]
+        Eindex = hands[1]
+        Sindex = hands[2]
+        Windex = hands[3]
     else:
-        create_lookup_table()
+        Nindex = set(range(13))
+        Sindex = set(range(14, 26))
 
-if os.path.exists("link_lookup_table.pkl"):
-    f = open("link_lookup_table.pkl", 'rb')
-    link_lookup_table = pickle.load(f)
-    f.close()
-else:
-    link_lookup_table = dict()
+    print("S")
+    print(hand_to_string(Sindex))
+    print("N")
+    print(hand_to_string(Nindex))
 
-card_holder = np.empty(52, dtype=int)
-card_holder.fill(-1)
-card_rank = np.arange(52).tolist()
-card_suit = np.arange(4).tolist()
-for j in Nindex:
-    card_holder[j] = 0
-for j in Eindex:
-    card_holder[j] = 1
-for j in Sindex:
-    card_holder[j] = 2
-for j in Windex:
-    card_holder[j] = 3
-suit_level_links = np.resize(card_holder, (4, 13)).tolist()
-suit_level_links = dict(zip(card_suit, suit_level_links))
-card_holder_dict = dict(zip(card_rank, card_holder.tolist()))
-play = [-1] * 52
-
-# Case 1: Trump = NT
-# print(type(Windex), type(Sindex), type(Eindex), type(Nindex))
-current_state = [Nindex, Eindex, Sindex, Windex]
-start = time.time()
-print(MAX_VALUE(state=current_state, trump=5), "NT")
-# Case 2：Trump = C
-start = time.time()
-print(MAX_VALUE(state=current_state, trump=1), "C")
-# Case 3: Trump = D
-start = time.time()
-print(MAX_VALUE(state=current_state, trump=2), "D")
-# Case 4: Trump = H
-start = time.time()
-print(MAX_VALUE(state=current_state, trump=3), "H")
-# Case 5: Trump = S
-start = time.time()
-print(MAX_VALUE(state=current_state, trump=4), "S")
-print()
+    if INPUT_METHOD != 1:
+        RC = RC - Nindex
+        RC = RC - Sindex
+        RC = RC
+        RC2 = RC.copy()
+        RC = np.array(list(RC2))
+        Windex = np.random.choice(a=RC, size=13, replace=False)
+        Windex.sort()
+        Windex = Windex.tolist()
+        Windex = set(Windex)
+        RC = set(RC) - Windex
+        Eindex = RC
 
 
-pickle_dump_link_lookup_table()
+    print("W")
+    print(hand_to_string(Windex))
+    print("E")
+    print(hand_to_string(Eindex))
+    if TRICK_LOOKUP_TABLE:
+        if os.path.exists("trick_lookup_table.npy"):
+            trick_lookup_table = np.load("trick_lookup_table.npy")
+        else:
+            create_lookup_table()
+
+    if DETAILED_LINK_OBJ:
+        if os.path.exists("detailed_link_lookup_table.pkl"):
+            f = open("detailed_link_lookup_table.pkl", 'rb')
+            link_lookup_table = pickle.load(f)
+            f.close()
+        else:
+            link_lookup_table = dict()
+    else:
+        if os.path.exists("compressed_link_lookup_table.pkl"):
+            f = open("compressed_link_lookup_table.pkl", 'rb')
+            link_lookup_table = pickle.load(f)
+            f.close()
+        else:
+            link_lookup_table = dict()
+
+    card_holder = np.empty(52, dtype=int)
+    card_holder.fill(-1)
+    card_rank = np.arange(52).tolist()
+    card_suit = np.arange(4).tolist()
+    for j in Nindex:
+        card_holder[j] = 0
+    for j in Eindex:
+        card_holder[j] = 1
+    for j in Sindex:
+        card_holder[j] = 2
+    for j in Windex:
+        card_holder[j] = 3
+    suit_level_links = np.resize(card_holder, (4, 13)).tolist()
+    suit_level_links = dict(zip(card_suit, suit_level_links))
+    card_holder_dict = dict(zip(card_rank, card_holder.tolist()))
+    play = [-1] * 52
+
+    # Case 1: Trump = NT
+    # print(type(Windex), type(Sindex), type(Eindex), type(Nindex))
+    current_state = [Nindex, Eindex, Sindex, Windex]
+    start = time.time()
+    print(MAX_VALUE(state=current_state, trump=5), "NT")
+    # Case 2：Trump = C
+    start = time.time()
+    print(MAX_VALUE(state=current_state, trump=1), "C")
+    # Case 3: Trump = D
+    start = time.time()
+    print(MAX_VALUE(state=current_state, trump=2), "D")
+    # Case 4: Trump = H
+    start = time.time()
+    print(MAX_VALUE(state=current_state, trump=3), "H")
+    # Case 5: Trump = S
+    start = time.time()
+    print(MAX_VALUE(state=current_state, trump=4), "S")
+    print()
+
+
+    pickle_dump_link_lookup_table()
+
+profile.run('main()')
--- link_lookup_table_watcher.py
+++ link_lookup_table_watcher.py
@@ -1,93 +1,100 @@
 import pickle
 LINK_LEVEL = 7 # number of remaining tricks to be stored - 1
 HASH_MOD = [64,65536,536870912,8589934592,4611686018427387904,4611686018427387904,4611686018427387904,4611686018427387904]
-
+DETAILED_LINK_OBJ = True # if links are stored
 
 class SuitLink:
     def __init__(self, link):
-        self.compressed_link = list(filter(lambda x: x != -1, link))
-        self.cache_valid = False
-        self.cache_hash = self.__hash__()
-        self.cache_valid = True
+        if DETAILED_LINK_OBJ:
+            self.compressed_link = list(filter(lambda x: x != -1, link))
+            self.cache_hash = self.hashing(self.compressed_link)
+        else:
+            compressed_link = list(filter(lambda x: x != -1, link))
+            self.cache_hash = self.hashing(compressed_link)
 
     def __str__(self):
-        return str(self.compressed_link)
+        return str(self.compressed_link) if DETAILED_LINK_OBJ else str(self.cache_hash)
+
+    @staticmethod
+    def hashing(comp_link):
+        hashed = 0
+        for i in comp_link:
+            hashed = hashed * 4 + i
+        return hashed * 17 + len(comp_link) - 1
 
     def __hash__(self):  # different hash => different link
-        if self.cache_valid:
-            return self.cache_hash
-        hashed = 0
-        for i in self.compressed_link:
-            if i != -1:
-                hashed = hashed * 4 + i
-        self.cache_hash = hashed * 13 + len(self.compressed_link) - 1
-        self.cache_valid = True
         return self.cache_hash
 
     def __eq__(self, other):
-        if self.cache_valid and other.cache_valid:
-            if self.cache_hash == other.cache_hash:
-                assert self.compressed_link == other.compressed_link
-                return True
-            else:
-                assert self.compressed_link != other.compressed_link
-                return False
-        else:
-            return self.compressed_link == other.compressed_link
+        return self.cache_hash == other.cache_hash
 
+    def __bool__(self):
+        return self.cache_hash != -1
+
+    def __len__(self):
+        return (self.cache_hash+1) % 17
 
 class SuitLevelLinks:
     def __init__(self, link_dict, trump, leader=0):
-        self.cache_valid = False
-        self.trump = trump
         if leader != 0:
-            self.link_dict = set()
+            links = set()
+            link_trump = SuitLink([])
             for i, link in link_dict.items():
                 if i == trump - 1:
-                    self.link_trump = SuitLink([(x - leader) % 4 for x in link if x != -1])
+                    link_trump = SuitLink([(x - leader) % 4 for x in link if x != -1])
                 else:
-                    self.link_dict.add(SuitLink([(x - leader) % 4 for x in link if x != -1]))
+                    links.add(SuitLink([(x - leader) % 4 for x in link if x != -1]))
         else:
-            self.link_dict = {SuitLink(y) for x, y in link_dict.items() if x != trump - 1}
-            if trump != 5:
-                self.link_trump = SuitLink(link_dict[trump-1])
-        self.cache_link_hash = {x.__hash__() for x in self.link_dict}  # need to be alwsys valid
-        if trump != 5:
-            self.cache_trump_hash = self.link_trump.__hash__()  # need to be always valid
-        self.cache_hash = self.__hash__()
-        self.cache_valid = True
+            links = {SuitLink(y) for x, y in link_dict.items() if x != trump - 1}
+            link_trump = SuitLink(link_dict[trump-1]) if trump != 5 else SuitLink([])
+        if DETAILED_LINK_OBJ:
+            self.links = (links, link_trump)
+        self.cache_links_hash = ({x.__hash__() for x in links}, link_trump.__hash__())
+        self.cache_hash = self.hashing(self.cache_links_hash)
 
     def __str__(self):
-        return str(self.link_dict) + str(self.trump) + "" if self.trump == 5 else str(self.link_trump)
+        if DETAILED_LINK_OBJ:
+            result = "{"
+            for i in self.links[0]:
+                result += str(i)
+                result += ', '
+            result += '}'
+            return result + " trump:" + str(self.links[1])
+        else:
+            return str(self.cache_links_hash)
+
+    @staticmethod
+    def hashing(links_hash):
+        hashed = links_hash[1]
+        for i in sorted(links_hash[0]):
+            hashed = (hashed * 872415239 + i) % HASH_MOD[LINK_LEVEL]  # it is a prime above max hash of link
+        return hashed
 
     def __hash__(self):
-        if self.cache_valid:
-            return self.cache_hash
-        else:
-            hashed = 0
-            if self.trump != 5:
-                hashed += self.cache_trump_hash
-            for i in sorted(self.cache_link_hash):
-                hashed = (hashed * 872415239 + i) % HASH_MOD[LINK_LEVEL]  # it is a prime above max hash of link
-            if self.trump == 5:
-                hashed *= 3
-            self.cache_hash = hashed
-            self.cache_valid = True
-            return hashed
+        return self.cache_hash
 
     def __eq__(self, other):
-        if self.trump != other.trump:
+        if self.cache_hash != other.cache_hash:
             return False
-        if self.cache_valid and other.cache_valid and self.cache_hash != other.cache_hash:
-            return False
-        if self.cache_link_hash != other.cache_link_hash:
-            return False
-        if self.trump != 5:
-            if self.cache_trump_hash != other.cache_trump_hash:
-                return False
-        return True
+        return self.cache_links_hash == other.cache_links_hash
 
-with open("link_lookup_table.pkl", 'rb') as f:
-    link_lookup_table = pickle.load(f)
-    it = iter(link_lookup_table)
-    print(len(link_lookup_table))
+    def __len__(self):
+        result = 0
+        for i in self.cache_links_hash[0]:
+            result += (i+1) % 17
+        return result + (1 + self.cache_links_hash[1]) % 17
+
+
+if DETAILED_LINK_OBJ:
+    with open("detailed_link_lookup_table.pkl", 'rb') as f:
+        link_lookup_table = pickle.load(f)
+        it = iter(link_lookup_table)
+        while True:
+            a = it.__next__()
+            if len(a) % 4 != 0:
+                print(len(a), a)
+else:
+    with open("compressed_link_lookup_table.pkl", 'rb') as f:
+        link_lookup_table = pickle.load(f)
+        two_card_table = set(map(lambda x: len(x), link_lookup_table.keys()))
+        print(two_card_table)
